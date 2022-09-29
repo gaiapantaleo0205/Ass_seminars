d9657708c6cef574430350429cd5d1b3a0d8c112
gaia_pantaleo <99889725+gaiapantaleo0205@users.noreply.github.com>
2022-09-29 16:31:19 +0200

    Add files via upload

---
+++ seminars.py
@@ -0,0 +1,39 @@
+
+def lower_case(string):
+    """Argument:
+    string -- text you want to turn into lower case
+
+    Returns:
+    lower_string -- lower case version of string
+
+    """
+
+    ### your code starts here
+    lower_string = lower(string)
+
+    ### your code ends here
+
+    return lower_string
+
+
+"""
+Do the same thing again with upper_case, but change the order so everyone
+experiences at least 1 merge conflict.
+"""
+
+def upper_case(string):
+    """
+    Argument:
+    string -- text you want to turn into upper case
+
+    Returns:
+    upper_string -- upper case version of string
+
+    """
+
+    ### your code starts here
+    upper_string = upper(string)
+
+    ### your code ends here
+
+    return upper_string
